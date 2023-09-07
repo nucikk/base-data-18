20a1de3d6eb54f72e076801c28cba22d5c24fb9e
nucikk <kirianuci@gmail.com>
2023-09-07 13:25:39 +0400

    add visual error message

--- base.js
+++ base.js
@@ -3,6 +3,7 @@
 async function fetchColorData() {
     const loadingAnimationContainer = document.getElementById('loadingAnimation');
     const dataList = document.getElementById('colorDataList');
+    const errorContainer = document.createElement('div'); 
   
     //* load and play the animation
     const animation = lottie.loadAnimation({
@@ -17,16 +18,18 @@ async function fetchColorData() {
   
     setTimeout(async () => {
       try {
-        const response = await fetch('https://reqres.in/api/unknown');  //* მონაცემების წამოღება  (Fetch data API)
-        if (!response.ok) throw new Error(`HTTP Error: ${response.status}`); //* შეამოწმებს სწორად მიიღო მოანებები თუ არა მაშინ დაბეჭდავს ერორის სტატუს კოდს
-        const { data } = await response.json(); //JSONდან ამოღებულია data 
+        const response = await fetch('https://reqres.in/api/unknown')  //* მონაცემების წამოღება  (Fetch data API)
+        if (!response.ok) throw new Error(`HTTP Error: ${response.status}`); //* შეამოწმებს სწორად მიიღო მონაცემები თუ არა მაშინ დაბეჭდავს ერორის სტატუს კოდს
+        const { data } = await response.json() //JSONდან ამოღებულია data 
   
         //* Stop and hide the loading animation
         animation.stop();
         loadingAnimationContainer.style.display = 'none';
-  
+        errorContainer.innerHTML = '';
         //* მომნაცემების ამოღება forEach მეთოდით, 
-        //*1.ელმენეტების შექმნა/ჩამატება ლისტში, 2.background ფერის მინიჭება data წამორებული ფერებით, 3. მონაცენების დაბეჭდვა
+        //* 1.ელმენეტების შექმნა/ჩამატება ლისტში
+        //* 2.background ფერის მინიჭება data წამორებული ფერებით
+        //* 3. მონაცენების დაბეჭდვა
         data.forEach(item => {
           const listItem = document.createElement('li');
           listItem.textContent = `Name: ${item.name}, Color: ${item.color}`;
@@ -34,6 +37,9 @@ async function fetchColorData() {
           dataList.appendChild(listItem);
         });
       } catch (error) {
+        errorContainer.textContent = `${error.message}`;
+        errorContainer.style.color = 'red';
+        dataList.appendChild(errorContainer);
         console.error('Error:', error);
       }
     }, 3000); //*timer 30 second(3000)
